34bdad45e093005691610ece4070d98096a68b23
Cody Green <cody@codygreen.com>
2019-08-07 09:41:43 -0700

    updated order of BIG-IP revoke license

--- deprovision.sh
+++ deprovision.sh
@@ -1,5 +1,6 @@
+if [ -z ${stackName} ]; then echo "stackName is unset" && exit 1; fi
+
 # delete S3 bucket content for BIG-IP clusters
-# aws s3api list-buckets --query "[Buckets][*][?contains(Name, '$stackName')].[Name]"
 for i in `aws s3api list-buckets --query "[Buckets][*][?contains(Name, '$stackName')].[Name]" | jq -r .[][][]`; 
 do 
     echo $i
@@ -7,21 +8,20 @@ do
     aws s3 rm s3://$i --recursive
 done
 
-# find CFT outputs
+# find BIG-IP management IP addresses, deprovision internal stacks before external stacks
+for ip in `aws cloudformation list-exports --query "Exports[?contains(Name, '$stackName')]|[?contains(Name, 'BIGIP2')]|[?contains(Name, 'Management')].[Value]" | jq -r .[][]`;
+do
+    echo "revoke license for $ip"
+    ssh -i $sshKey -oStrictHostKeyChecking=no admin@"$ip" 'modify cli preference pager disabled display-threshold 0; revoke sys license'
+done
 
+# deprovision external stack 
+for ip in `aws cloudformation list-exports --query "Exports[?contains(Name, '$stackName')]|[?contains(Name, 'BIGIP1')]|[?contains(Name, 'Management')].[Value]" | jq -r .[][]`;
+do
+    echo "revoke license for $ip"
+    ssh -i $sshKey -oStrictHostKeyChecking=no admin@"$ip" 'modify cli preference pager disabled display-threshold 0; revoke sys license'
+done
 
-# find BIG-IP management IP addresses
-# aws cloudformation list-exports --query 'Exports[?contains(Name, `cody-sca-test`)]'
-# aws cloudformation list-exports --query 'Exports[?contains(Name, `cody-sca-test`)]|[?contains(Name, `BIGIP`)]|[?contains(Name, `External`)].[Value]'
-# aws ec2 describe-network-interfaces --network-interface-id=eni-03bd3f82d34f9eed4
-# aws ec2 describe-network-interfaces --network-interface-id=eni-03bd3f82d34f9eed4 --query 'NetworkInterfaces[*].Association.[PublicIp]'
-
-# release BIG-IP licenses
-# for i in {1..4}
-# do
-#     echo "bigip$i";
-#     # echo $bigip1
-#     ip=bigip${i}
-#     echo "${!ip}"
-#     ssh -i $sshKey -oStrictHostKeyChecking=no admin@"${!ip}" 'modify cli preference pager disabled display-threshold 0; revoke sys license'
-# done
+# delete CFT stack
+echo "deleting CFT stack $stackName"
+aws cloudformation delete-stack --stack-name $stackName
