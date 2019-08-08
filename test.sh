1281c0b54e8c156f69215ab1cd94f2e7a22af99e
Cody Green <cody@codygreen.com>
2019-08-08 12:00:01 -0700

    configure iapp - step 1

--- test.sh
+++ test.sh
@@ -25,5 +25,14 @@ echo $bigip1_pwd
 for ip in `aws cloudformation list-exports --query "Exports[?contains(Name, '$stackName')]|[?contains(Name, 'BIGIP1')]|[?contains(Name, 'Management')].[Value]" | jq -r .[][]`;
 do
     echo "install HA iApp for Tier-1 BIG-IP $ip"
-    ssh -i $sshKey -oStrictHostKeyChecking=no admin@"$ip" "load sys application template /config/cloud/aws/f5.aws_advanced_ha.v1.4.0rc3.tmpl"
+    ssh -i $sshKey -oStrictHostKeyChecking=no admin@"$ip" "create /sys application service aws_HA template f5.aws_advanced_ha.v1.4.0rc3 tables add { subnet_routes__cidr_blocks { column-names { route_table_id dest_cidr_block } rows { { row { "rtb-06ed70ecb5a149f64" "0.0.0.0/0" } } } } } variables add { subnet_routes__route_management { value yes }  }" 
 done
+
+# below line is from across AZ iApp -don't use, for notes only
+#"create /sys application service HA_Across_AZs template f5.aws_advanced_ha.v1.4.0rc3 tables add { eip_mappings__mappings { column-names { eip az1_vip az2_vip } rows { { row { ${VIPEIP} /Common/${EXTPRIVIP} /Common/${PEER_EXTPRIVIP} } } } } } variables add { eip_mappings__inbound { value yes } }\"\n",
+
+
+
+
+
+#"load sys application template /config/cloud/aws/f5.aws_advanced_ha.v1.4.0rc3.tmpl"
