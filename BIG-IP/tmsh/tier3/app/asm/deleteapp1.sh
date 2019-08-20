8251790e98d35a52b049b5ad8b62085c39b56dc9
vinnie357 <vinnie.mazza@gmail.com>
2019-08-20 18:15:30 -0400

    add bot defense and logging to tier3

--- BIG-IP/tmsh/tier3/app/asm/deleteapp1.sh
+++ BIG-IP/tmsh/tier3/app/asm/deleteapp1.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 # partition
 partition="app1"
+# app name
+appName="app1"
 # nodes
 node1="5.6.7.8"
 # virtual addresss
@@ -12,12 +14,14 @@ asmFile="/config/owasp-auto-tune.xml"
 
 # remove items
 echo  -e 'create cli transaction;
-delete ltm virtual /'${partition}'/app1_http;
-delete ltm virtual /'${partition}'/app1_https;
-delete ltm pool /'${partition}'/app1_https_pool;
+delete ltm virtual /'${partition}'/'${appName}'_http;
+delete ltm virtual /'${partition}'/'${appName}'_https;
+delete ltm pool /'${partition}'/'${appName}'_https_pool;
 delete ltm node /'${partition}'/'${node1}';
-delete ltm policy /'${partition}'/app1_asm_policy_https;
+delete ltm policy /'${partition}'/'${appName}'_asm_policy_https;
 delete asm policy owasp-auto-tune;
+delete security log profile /'${partition}'/'${appName}'_sec_log
+delete security bot-defense profile /'${partition}'/'${appName}'_bot
 submit cli transaction' | tmsh -q
 
 # remove partition
