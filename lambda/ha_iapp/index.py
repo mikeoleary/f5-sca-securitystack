15290ec2b6211f7eb4e36c9ae29c8a74d3e3871f
Cody Green <cody@codygreen.com>
2019-08-21 21:43:10 -0500

    fixed lambda packaging issues

--- lambda/ha_iapp/index.py
+++ lambda/ha_iapp/index.py
@@ -2,26 +2,30 @@ from f5_sca_libs import bigip
 from f5_sca_libs import password
 import boto3
 import json
+import logging
 
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
 
-    # obtain required variables
-    mgmt_ip = event['Records'][0]['bigip']['mgmt_ip']
-    iapp_url = event['Records'][0]['bigip']['iapp_url']
-    route_table_id = event['Records'][0]['bigip']['route_table_id']
-    interface = event['Records'][0]['bigip']['interface']
-    s3_bucket = event['Records'][0]['bigip']['s3_bucket']
+    if event['RequestType'] == 'Create':
+        # obtain required variables
+        mgmt_ip = event['ResourceProperties'].get('mgmt_ip')
+        iapp_url = event['ResourceProperties'].get('iapp_url')
+        route_table_id = event['ResourceProperties'].get('route_table_id')
+        interface = event['ResourceProperties'].get('interface')
+        s3_bucket = event['ResourceProperties'].get('s3_bucket')
 
-    if not mgmt_ip or not iapp_url or not route_table_id or not interface or not s3_bucket:
-        raise Exception(f'ERROR: missing required event attributes {json.dumps(event, indent=2)} ')
+        if not mgmt_ip or not iapp_url or not route_table_id or not interface or not s3_bucket:
+            raise Exception(f'ERROR: missing required event attributes {json.dumps(event, indent=2)} ')
 
-    # obtain BIG-IP Password
-    s3_client = boto3.client('s3')
-    password = password.get(s3_client, s3_bucket)
+        # obtain BIG-IP Password
+        s3_client = boto3.client('s3')
+        pwd = password.get(s3_client, s3_bucket)
+        if not pwd:
+            raise Exception('ERROR: unable to obtain BIG-IP password')
 
-    # deploy and configure ha_iapp
-    client = bigip.client(mgmt_ip, 'admin', password)
-    bigip.install_iapp(client, iapp_url)
-    iapp_name = iapp_url.split('/')[-1].replace('.tmpl', '')
-    bigip.cfg_ha_iapp(client, iapp_name, route_table_id, interface)
+        # deploy and configure ha_iapp
+        client = bigip.client(mgmt_ip, 'admin', pwd)
+        bigip.install_iapp(client, iapp_url)
+        iapp_name = iapp_url.split('/')[-1].replace('.tmpl', '')
+        bigip.cfg_ha_iapp(client, iapp_name, route_table_id, interface)
