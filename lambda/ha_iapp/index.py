b1c761b998c6620533c91d1c23f87c47e89b126c
Cody Green <cody@codygreen.com>
2019-08-27 14:30:40 -0500

    fix timeout issue when cfnreponse is not called

--- lambda/ha_iapp/index.py
+++ lambda/ha_iapp/index.py
@@ -6,7 +6,9 @@ import logging
 import cfnresponse
 
 def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.info('Received event: ' + json.dumps(event, indent=2))
 
     if event['RequestType'] == 'Create':
         # obtain required variables
@@ -19,19 +21,27 @@ def lambda_handler(event, context):
         if not mgmt_ip or not iapp_url or not route_table_id or not interface or not s3_bucket:
             raise Exception(f'ERROR: missing required event attributes {json.dumps(event, indent=2)} ')
 
-        # obtain BIG-IP Password
-        s3_client = boto3.client('s3')
-        pwd = password.get(s3_client, s3_bucket)
-        if not pwd:
-            raise Exception('ERROR: unable to obtain BIG-IP password')
+        # make sure we always return a reponse to CFN
+        try:
+            # obtain BIG-IP Password
+            s3_client = boto3.client('s3')
+            pwd = password.get(s3_client, s3_bucket)
+            if not pwd:
+                raise Exception('ERROR: unable to obtain BIG-IP password')
 
-        # deploy and configure ha_iapp
-        client = bigip.client(mgmt_ip, 'admin', pwd)
-        bigip.install_iapp(client, iapp_url)
-        iapp_name = iapp_url.split('/')[-1].replace('.tmpl', '')
-        bigip.cfg_ha_iapp(client, iapp_name, route_table_id, interface)
+            # deploy and configure ha_iapp
+            client = bigip.client(mgmt_ip, 'admin', pwd)
+            bigip.install_iapp(client, iapp_url)
+            iapp_name = iapp_url.split('/')[-1].replace('.tmpl', '')
+            bigip.cfg_ha_iapp(client, iapp_name, route_table_id, interface)
 
-        cfnresponse.send(event, context, cfnresponse.SUCCESS, True)
+            logger.info('Successfully updated the HA iApp')
+            cfnresponse.send(event, context, cfnresponse.SUCCESS, True)
+            return
+            
+        except Exception:
+            logger.exception('Signaling failure to CloudFormation.')
+            cfnresponse.send(event, context, cfnresponse.FAILED, {})
 
     if event['RequestType'] == 'Delete':
         cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
